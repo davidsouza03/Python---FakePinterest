6a2d6759b1df48e570fe22aababdb764c0296931
davidssilva03 <davidsouza183@gmai.com>
2024-04-12 15:41:42 -0300

    nova mudanca parte quatro

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "fc80be3a6cc11d971a245f733ec201bb"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
@@ -15,3 +15,4 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
 
+from fakepinterest import routes
