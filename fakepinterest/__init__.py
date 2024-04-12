8b0533d852373e9b4b80e5887fbcbb214c933286
davidssilva03 <davidsouza183@gmai.com>
2024-04-11 22:08:23 -0300

    Primeiro commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "fc80be3a6cc11d971a245f733ec201bb"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+from fakepinterest import routes
---
+++ main.py
@@ -0,0 +1,6 @@
+from fakepinterest import app
+
+
+# Se executar o arquivo main.py é para rodar esse codigo.
+if __name__ == '__main__':
+    app.run(debug=True)
